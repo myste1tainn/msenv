c5bb09896565fddd77abf394960e38032d86835e
Arnon Keereena <arnon.keereena@accenture.com>
2022-10-19 23:39:40 +0700

    First commit

---
+++ Init.go
@@ -0,0 +1,49 @@
+package nextenv
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+func Init() {
+	viper.AddConfigPath("./configs")
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Println("Cannot read config.yaml with error: ", err)
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Println("Configfile ConfigFileNotFoundError: ", err)
+		} else {
+			log.Println("Configfile format : ", err)
+		}
+	}
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	for _, key := range viper.AllKeys() {
+		val := viper.Get(key)
+		viper.Set(key, val)
+	}
+
+	if isDevMode {
+		for i, k := range viper.AllKeys() {
+			fmt.Println(i, " ==> ", k, " ==> ", viper.Get(k))
+		}
+	}
+}
+
+var isDevMode = false
+
+func EnableDevMode() {
+	isDevMode = true
+}
+
+func DisableDevMode() {
+	isDevMode = false
+}
